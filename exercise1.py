e222ee3a518be4b8e28be0bda9eebf6f66dcf049
CesarOrtiz <000025591@alumno.utel.edu.mx>
2023-05-01 05:14:46 -0600

    agregar el codigo del ejercicio

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+numero = (input("Dame un Numero: "))
+print(numero)
+print(type(numero))
+
+numeroentero = int(numero)
+print(type(numeroentero))
+
+division = numeroentero / 3
+print("division: ", division)
+
+print(division > 2)
